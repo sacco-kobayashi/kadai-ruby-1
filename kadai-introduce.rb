d0092fce9d12d35cc41732e7ade91f2e897aa702
sana.kobayashi <sa09na21@gmail.com>
2021-11-27 16:39:12 +0000

    add ruby-kadai-saiteisyutu

--- kadai-introduce.rb
+++ kadai-introduce.rb
@@ -1,14 +1,6 @@
 sei = '田中'
 mei = '太郎'
-desu = 'です。'
-
-fullname = sei + mei + desu
 
 age = '25'
-saidesu = '歳です。'
 
-nenrei = age + saidesu
-
-jikosyokai = fullname + nenrei 
-
-puts jikosyokai
+puts sei + mei + "です。" + age + "歳です。"
